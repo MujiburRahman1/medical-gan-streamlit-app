dde876a491e034393df1df417dd4619d0ca89c5b
Mujib ur Rahman <mujibkhan081@gmail.com>
2025-08-13 23:07:25 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,7 +8,7 @@ to generate synthetic medical records with privacy guarantees.
 import streamlit as st
 import pandas as pd
 import numpy as np
-import torch
+
 import plotly.graph_objects as go
 import plotly.express as px
 from plotly.subplots import make_subplots
